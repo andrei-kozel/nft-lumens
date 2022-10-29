4d6bd4173359b6450dd8ca6b23a981495f5b9077
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-10-29 16:28:28 +0200

    z-index fix

--- components/Title.tsx
+++ components/Title.tsx
@@ -9,9 +9,9 @@ const Title = ({ text, color }: IProps) => {
   return (
     <div className="w-full flex justify-center items-center">
       <div className="relative">
-        <p className="py-10 text-4xl font-bold relative z-20">{text}</p>
+        <p className="py-10 text-4xl font-bold relative z-10">{text}</p>
         <span
-          className={`absolute w-[80px] h-[80px] rounded-full bg-[${color}] top-[20px] -left-[30px] z-10`}
+          className={`absolute w-[80px] h-[80px] rounded-full bg-[${color}] top-[20px] -left-[30px]`}
         />
       </div>
     </div>
