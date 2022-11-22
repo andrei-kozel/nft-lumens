c029fed1822fd706418913486cee39ba0e458423
Andrei Kozel <kozel.andrei.94@gmail.com>
2022-11-22 20:24:14 +0100

    how it works section fix

--- components/DashedLine.tsx
+++ components/DashedLine.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const DashedLine = () => (
   <div className="flex justify-center items-center rotate-90 md:rotate-0">
-    <span className="border hidden md:block w-3 mr-3 border-[#794187]" />
-    <span className="border hidden md:block w-3 mr-3 border-[#794187]" />
-    <span className="border hidden md:block w-3 mr-3 border-[#794187]" />
-    <span className="border hidden md:block w-3 mr-3 border-[#794187]" />
+    <span className="border hidden xl:block w-3 mr-3 border-[#794187]" />
+    <span className="border hidden xl:block w-3 mr-3 border-[#794187]" />
+    <span className="border hidden xl:block w-3 mr-3 border-[#794187]" />
+    <span className="border hidden xl:block w-3 mr-3 border-[#794187]" />
     <span className="border w-3 mr-3 border-[#794187]" />
     <span className="border w-3 mr-3 border-[#794187]" />
     <span className="border w-3 mr-3 border-[#794187]" />
--- components/HowItWorks.tsx
+++ components/HowItWorks.tsx
@@ -12,33 +12,33 @@ const HowItWorks = () => {
   return (
     <div className="container py-16 xl:py-20 m-auto relative">
       <Title text="How it works" bgColor="bg-[#E03054]" />
-      <div className="flex flex-col-reverse md:flex-row gap-[80px] md:gap-0 justify-around h-full mt-6 ">
+      <div className="flex flex-col md:flex-row gap-[80px] md:gap-0 justify-around h-full mt-6 ">
         <div className="flex flex-col justify-center items-center">
           <span className="text-lightPink text-4xl bg-[#372839] w-[65px] rounded-full h-[65px] flex justify-center items-center mb-4">
             <BiWalletAlt />
           </span>
-          <p className="text-yellow">Set Up Your Wallet</p>
+          <p className="text-yellow text-center">Set Up Your Wallet</p>
         </div>
         <DashedLine />
         <div className="flex flex-col justify-center items-center">
           <span className="text-lightPink text-4xl bg-[#372839] w-[65px] rounded-full h-[65px] flex justify-center items-center mb-4">
-            <BiWalletAlt />
+            <BiCollection />
           </span>
-          <p className="text-yellow">Set Up Your Wallet</p>
+          <p className="text-yellow  text-center">Create your collection</p>
         </div>
         <DashedLine />
         <div className="flex flex-col justify-center items-center">
           <span className="text-lightPink text-4xl bg-[#372839] w-[65px] rounded-full h-[65px] flex justify-center items-center mb-4">
-            <BiWalletAlt />
+            <BiAddToQueue />
           </span>
-          <p className="text-yellow">Set Up Your Wallet</p>
+          <p className="text-yellow text-center">Add your NFTs</p>
         </div>
         <DashedLine />
         <div className="flex flex-col justify-center items-center">
           <span className="text-lightPink text-4xl bg-[#372839] w-[65px] rounded-full h-[65px] flex justify-center items-center mb-4">
-            <BiWalletAlt />
+            <BiBarChart />
           </span>
-          <p className="text-yellow">Set Up Your Wallet</p>
+          <p className="text-yellow text-center">List them for sale</p>
         </div>
       </div>
       <div className="hiw-blur" />
